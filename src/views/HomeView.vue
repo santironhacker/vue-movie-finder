846bc8688bc2fd201cc60cd9b7998be10d696401
santironhacker <santi92.network@gmail.com>
2020-11-05 15:22:05 +0700

    Added instructions content to HomeView. Added background picture and scoped styles.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,47 @@
 <template>
-  <h1>This is the Home View</h1>
+  <section class="section-1">
+    <h2>Movie Finder</h2>
+    <div>
+      <p>
+        It's happen to all of us before. After an exciting conversation with your pals, you come back home thinking:
+      </p>
+      <q>I must see this movie!</q>
+      <p>
+        However, next morning when you wake up you forgot what was all about. 
+        <strong>Movie Finder</strong> watches after your memory to keep track of your friend's best picks.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
+  name: 'HomeView'
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../styles/global.scss";
 
+.section-1 {
+  /* Set background style */
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4)), url("../assets/movie-rolls-original.jpg") no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+
+  /* Set rules to fill background */
+  min-height: 90vh;
+  min-width: 100%;
+	
+  /* Set up proportionate scaling */
+  width: 100%;
+  height: auto;
+}
+
+q {
+  font-style: italic;
+  font-size: 18px;
+}
 </style>
