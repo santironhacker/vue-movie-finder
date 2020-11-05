b458e5f354cf607cf55cd6a036a025de74b3d9b1
santironhacker <santi92.network@gmail.com>
2020-11-05 15:48:52 +0700

    Created BaseCard ui component and registered it as a general component in main.js.

---
+++ src/components/ui/BaseCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/global.scss";
+
+.card {
+  margin: 0 auto;
+  padding: 2rem;
+  width: 60%;
+  background-color: $background-primary;
+  opacity: 0.9;
+  border: solid 1px $primary-color;
+  border-radius: 20px;
+}
+</style>
