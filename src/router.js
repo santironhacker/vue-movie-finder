08584a5fbf1ac190003c8b32b3420f393708e691
santironhacker <santi92.network@gmail.com>
2020-11-05 12:32:56 +0700

    Fixed router indentation

--- src/router.js
+++ src/router.js
@@ -3,18 +3,18 @@ import HomeView from './views/HomeView.vue';
 import MoviesView from './views/MoviesView.vue';
 
 const router = createRouter({
-        history: createWebHistory(),
-        routes: [
-            {
-                path: '/', redirect: '/home'
-            },
-            {
-                path: '/home', component: HomeView
-            },
-            {
-                path: '/movies', component: MoviesView, props: true
-            }
-        ]
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/', redirect: '/home'
+    },
+    {
+      path: '/home', component: HomeView
+    },
+    {
+      path: '/movies', component: MoviesView, props: true
+    }
+  ]
 });
 
 export default router;
