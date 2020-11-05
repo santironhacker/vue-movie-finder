a24b5e7c2befa722e80a5c02a9dc21fbf606c5f2
santironhacker <santi92.network@gmail.com>
2020-11-05 19:39:41 +0700

    Created MovieItem component to display the movie details inside the slider. Wired component into MoviesSlider. Added a default image in case img src fails to load.

---
+++ src/components/movies/MovieItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <article>
+    <ul>
+      <li>
+        <strong>Title: </strong>
+        <span>{{ title }}</span>
+      </li>
+      <li>
+        <strong>Directed by: </strong>
+        <span>{{ director }}</span>
+      </li>
+      <li>
+        <strong>Year: </strong>
+        <span>{{ year }}</span>
+      </li>
+    </ul>
+    <img
+      :src="posterUrl"
+      :alt="imgDescription"
+      @error="setAltImg"
+      width="300"
+      height="444"
+    >
+  </article>
+</template>
+
+<script>
+export default {
+  props: [
+    'id',
+    'title',
+    'director',
+    'year',
+    'posterUrl'
+  ],
+  methods: {
+    setAltImg (event) {
+      event.target.src = require('@/assets/img-not-available.png')
+    }
+  },
+  computed: {
+    imgDescription () {
+      return `Poster from the movie ${this.title}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
+  max-width: 300px;
+  color: $secondary-color;
+
+  strong {
+    color: $background-primary;
+  }
+}
+
+img[alt~="Poster"] {
+  padding: 1rem;
+  max-width: 300px;
+}
+</style>
--- src/components/movies/MoviesSlider.vue
+++ src/components/movies/MoviesSlider.vue
@@ -6,7 +6,13 @@
         v-for="movie in movies"
         :key="movie.imdbID"
       >
-        <span>Slide content</span>
+        <movie-item      
+          :id="movie.imdbID"
+          :title="movie.Title"
+          :director="movie.Type"
+          :year="movie.Year"
+          :posterUrl="movie.Poster"
+        ></movie-item>
       </div>
     </div>
     <div class="swiper-button-prev"></div>
@@ -18,57 +24,52 @@
 import SwiperCore, { EffectCoverflow, Navigation, Pagination } from 'swiper';
 import { Swiper } from 'swiper';
 import 'swiper/swiper-bundle.css';
+import MovieItem from './MovieItem.vue';
 
 SwiperCore.use([EffectCoverflow, Navigation, Pagination]);
 
 export default {
+  components: {
+    MovieItem 
+  },
   data() {
-      return {
-          swiper: null
-      }
+    return {
+      swiper: null
+    }
   },
   props: [
-      'movies'
+    'movies'
   ],
   methods: {
-      onSwiper(swiper) {
-          console.log(swiper)
-      },
-      onSlideChange() {
-          console.log('slide change')
-      },
-      imgUrl(posterUrl) {
-        return `background:url(${posterUrl} no-repeat center center fixed)`;
-      }
+    imgUrl(posterUrl) {
+      return `background:url(${posterUrl} no-repeat center center fixed)`;
+    }
   },
   mounted() {
     this.swiper = new Swiper('.swiper-container', {
-        effect: 'coverflow',
-        grabCursor: true,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        observer: true,
-        observeParents: true,
-        coverflowEffect: {
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: false,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        a11y: {
-            prevSlideMessage: 'Previous slide',
-            nextSlideMessage: 'Next slide',
-            firstSlideMessage: 'This is the first slide',
-            lastSlideMessage: 'This is the last slide'
-        },
-    });
-    this.swiper.on('slideChange', function () {
-        console.log('slide changed');
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      observer: true,
+      observeParents: true,
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      a11y: {
+        prevSlideMessage: 'Previous slide',
+        nextSlideMessage: 'Next slide',
+        firstSlideMessage: 'This is the first slide',
+        lastSlideMessage: 'This is the last slide'
+      },
     });
   }
 }
