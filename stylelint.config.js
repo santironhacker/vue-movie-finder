016536b5bd883314fb72ecd430016e5fbc3ad249
santironhacker <santi92.network@gmail.com>
2020-11-06 12:28:30 +0700

    Installed processor-arbitrary-tags and config-recess-order to improve linting of scss code inside the vue files and map the correct line for errors.

---
+++ stylelint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  processors: [
+    ["@mapbox/stylelint-processor-arbitrary-tags", { fileFilterRegex: [/\.vue$/] }]
+  ],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order"
+  ],
+  rules: {
+    "no-empty-source": null,
+    "at-rule-no-unknown": [
+      true,
+      {
+        "ignoreAtRules": [
+          "include"
+        ]
+      }
+    ]
+  }
+};
