070ff0dc3b33923afaa941621923d6b1e69f61ed
santironhacker <santi92.network@gmail.com>
2020-11-16 13:36:42 +0700

    Fixed api url to https

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://www.omdbapi.com/',
+  baseURL: 'https://www.omdbapi.com/',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
